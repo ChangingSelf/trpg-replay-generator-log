ce8f3cb092c04cff6efd6bb4a4c7293c924c9d0d
ChangingSelf <ChangingSelf@163.com>
2022-11-25 16:29:21 +0800

    v1.15.1

--- src/providers/CompletionItemProvider.ts
+++ src/providers/CompletionItemProvider.ts
@@ -7,12 +7,12 @@ import { RegexUtils } from '../utils/RegexUtils';
 export class CompletionItemProvider implements vscode.CompletionItemProvider {
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>> {
         let range = document.getWordRangeAtPosition(position.translate(0,-1),/[^,.\n\[\]\(\)]+/);
-        if(!range){
+        if(!range && context.triggerCharacter !== '[' && context.triggerCharacter !== ','){
             //如果找不到单词
             return [];
         }
         let pcName = document.getText(range);
-        let prefix = document.getText(new vscode.Range(new vscode.Position(position.line,0),position));
+        let prefix = document.getText(new vscode.Range(new vscode.Position(position.line,0),position));//触发位置这一行前头的内容
         let result:vscode.CompletionItem[] = [];
         //读取配置
         let settings = loadSettings();
@@ -51,6 +51,20 @@ export class CompletionItemProvider implements vscode.CompletionItemProvider {
                     audioList.forEach(medium=>{
                         result.push({label:medium.mediaName,insertText:medium.mediaName});
                     });
+                }
+                break;
+            case "["://角色名补全
+                if(hasCharacterPath && !range){//只有在行开头时才补全
+                    for(let name of pcMap.keys()){                    
+                        result.push({label:name,insertText:name});
+                    }
+                }
+                break;
+            case ","://多角色输入补全
+                if(hasCharacterPath && range && /^\[[^\]]+,/m.test(prefix)){//只有不在行开头时才补全，且触发点必须在框内
+                    for(let name of pcMap.keys()){                    
+                        result.push({label:name,insertText:name});
+                    }
                 }
                 break;
             default:
