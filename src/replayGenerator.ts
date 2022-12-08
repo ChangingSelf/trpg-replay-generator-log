b0a47c9402ebbeaba831106dc03eb76aa8744d50
ChangingSelf <ChangingSelf@163.com>
2022-12-08 22:24:21 +0800

    v1.20.3 输出路径二次确认

--- src/replayGenerator.ts
+++ src/replayGenerator.ts
@@ -51,18 +51,36 @@ export function exportVideo(){
 /**
  * 合成语音
  */
-export function synthesizedSpeech(){
+export async function synthesizedSpeech(){
 
     let s = utils.loadSettings(true);
 
     //运行
     
     let rplGenCorePathDir = path.dirname(s["rplGenCorePath"]);
+
+    //弹框修改输出路径
+    vscode.window.showInformationMessage("因为此操作输出文件较多，所以需要二次确认输出路径");
+    let outputPath =  await vscode.window.showOpenDialog({
+        "canSelectFolders":true,
+        "defaultUri":vscode.Uri.file(s.outputPath),
+        "title":"请确认输出路径",
+        "openLabel":"确认输出路径",
+    });
+    if(outputPath === undefined){
+        vscode.window.showInformationMessage("已取消操作");
+        return;
+    }else{
+        s.outputPath = outputPath[0].fsPath;
+        vscode.window.showInformationMessage(`输出路径已设置为${s.outputPath}`);
+    }
     
     
     terminal.sendText(`cd ${rplGenCorePathDir}`);
     terminal.sendText(`${s["rplGenCorePath"]} --Modules speech_synthesizer --LogFile ${s["logFile"]} --MediaObjDefine ${s["mediaObjDefine"]} --CharacterTable ${s["characterTable"]} --OutputPath ${s["outputPath"]} --AccessKey ${s["accessKey"]} --AccessKeySecret ${s["accessKeySecret"]} --Appkey ${s["appkey"]} --Azurekey ${s["azurekey"]} --ServRegion ${s["servRegion"]}`);
     terminal.show();
+
+    /*不再需要了
     vscode.window.showInformationMessage(`合成程序结束后，点击按钮可以打开处理后的log（先点一下这个消息框避免它自动消失）`,"打开AsteriskMarkedLogFile.rgl").then(selection=>{
         vscode.workspace.openTextDocument(path.join(s["outputPath"],"AsteriskMarkedLogFile.rgl"))
         .then(doc => {
@@ -73,20 +91,35 @@ export function synthesizedSpeech(){
             vscode.window.showErrorMessage(err);
         });
     });
-
+    */
 }
 
 /**
  * 导出XML，需要timeline文件
  */
- export function exportXML(){
+ export async function exportXML(){
 
     let s = utils.loadSettings(true);
 
     //运行
     
     let rplGenCorePathDir = path.dirname(s["rplGenCorePath"]);
-    
+
+    //弹框修改输出路径
+    vscode.window.showInformationMessage("因为此操作输出文件较多，所以需要二次确认输出路径");
+    let outputPath =  await vscode.window.showOpenDialog({
+        "canSelectFolders":true,
+        "defaultUri":vscode.Uri.file(s.outputPath),
+        "title":"请确认输出路径",
+        "openLabel":"确认输出路径",
+    });
+    if(outputPath === undefined){
+        vscode.window.showInformationMessage("已取消操作");
+        return;
+    }else{
+        s.outputPath = outputPath[0].fsPath;
+        vscode.window.showInformationMessage(`输出路径已设置为${s.outputPath}`);
+    }
     
     terminal.sendText(`cd ${rplGenCorePathDir}`);
     terminal.sendText(`${s["rplGenCorePath"]} --Modules export_xml --TimeLine ${s["timeLine"]} --LogFile ${s["logFile"]} --MediaObjDefine ${s["mediaObjDefine"]} --OutputPath ${s["outputPath"]} --FramePerSecond ${s["framePerSecond"]} --Width ${s["width"]} --Height ${s["height"]} --Zorder ${s["zorder"]}`);
